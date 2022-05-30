bac18ba38a8b181e65a0cd03996769f89c81ffce
Purinut <reawjangpurinut@gmail.com>
2022-05-30 13:10:17 +0700

    Update ReentrancyAttacker.tsx

--- webpage/components/ReentrancyAttacker.tsx
+++ webpage/components/ReentrancyAttacker.tsx
@@ -12,7 +12,7 @@ function ContractBalanceCard({balance, attack, isInitSuccess}: Props) {
   return (
     <Card className="md:w-auto w-full bg-[#EAEFC4] p-5 w-fit mx-auto">
       <Text h3 weight="bold" size={20} transform="uppercase" color="#346473aa" className='text-center'>
-        Balance in Attacker wallet
+        Balance in Attacker Contract
       </Text>
       <Text h2 weight="extrabold" size={40} color="#346473" className='text-center'>
           {balance} Wei
